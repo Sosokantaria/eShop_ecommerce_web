2a9a9e6ac89cc9c6c35b6cc974bd2493599e0c47
Sosokantaria <121603634+Sosokantaria@users.noreply.github.com>
2023-07-04 22:06:58 +0400

    ##

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -1,7 +1,5 @@
-
 import { NavForRouteViews } from "../../components/navForRoureViews";
 import { Address } from "../../components/address";
-
 
 export function Footer() {
   return (
@@ -9,13 +7,12 @@ export function Footer() {
       <div className="w-[90%] m-auto">
         <div className="w-full max-w-screen-xl mx-auto  items-center md:py-8">
           <div className="flex  flex-col gap-y-6 sm:flex  sm:items-center ">
-            <div className="mt-3 flex ">
-            </div>
+            <div className="mt-3 flex "></div>
             <div className="flex flex-wrap items-center gap-x-20 mb-6 font-medium sm:mb-0 ">
               <NavForRouteViews />
             </div>
             <div className="flex flex-wrap gap-x-4 m-4 text-sm font-bold  sm:mb-0 ">
-            <Address/>
+              <Address />
             </div>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto  lg:my-8" />
